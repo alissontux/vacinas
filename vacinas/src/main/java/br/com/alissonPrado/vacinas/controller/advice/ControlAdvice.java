0dae23ca88f511e08525ec9ea17d48a269980b36
alisson <alissontux@gmail.com>
2021-03-14 23:15:13 -0300

    Commit inicial

---
+++ vacinas/src/main/java/br/com/alissonPrado/vacinas/controller/advice/ControlAdvice.java
@@ -0,0 +1,37 @@
+package br.com.alissonPrado.vacinas.controller.advice;
+
+/*
+ @ControllerAdvice is a specialization of the @Component annotation which allows to handle exceptions 
+ across the whole application in one global handling component. It can be viewed as an interceptor of 
+ exceptions thrown by methods annotated with @RequestMapping and similar.
+ */
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+@ControllerAdvice
+public class ControlAdvice {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ResponseBody
+	public String handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+
+		return "Campo: " + ex.getBindingResult().getFieldError().getField() + " - Descrição erro: "
+				+ ex.getBindingResult().getFieldError().getDefaultMessage();
+	}
+
+	@ExceptionHandler(InvalidFormatException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ResponseBody
+	public String invalidFormatException(InvalidFormatException ex) {
+
+		return "Dados enviados em formato incorreto. " + ex.getLocalizedMessage();
+	}
+}
