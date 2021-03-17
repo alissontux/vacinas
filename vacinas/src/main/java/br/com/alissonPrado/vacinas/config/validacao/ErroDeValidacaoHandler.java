18c6d141fe482d780b2aa291bd04e95cd2d0e33e
alisson <alissontux@gmail.com>
2021-03-17 11:25:08 -0300

    Teste

--- vacinas/src/main/java/br/com/alissonPrado/vacinas/config/validacao/ErroDeValidacaoHandler.java
+++ vacinas/src/main/java/br/com/alissonPrado/vacinas/config/validacao/ErroDeValidacaoHandler.java
@@ -9,7 +9,6 @@ package br.com.alissonPrado.vacinas.config.validacao;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
